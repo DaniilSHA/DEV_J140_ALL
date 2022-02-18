627b977e52448a019b6b458acdc5adc46d096245
Daniil_Shakhmovedev <Daniil_Shakhmovedev@epam.com>
2022-02-18 21:17:44 +0300

    make autho tamplate commit

--- src/main/java/com/example/dev_j140_all/models/AuthorizationStage.java
+++ src/main/java/com/example/dev_j140_all/models/AuthorizationStage.java
@@ -1,7 +1,12 @@
 package com.example.dev_j140_all.models;
 
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -17,15 +22,71 @@ public class AuthorizationStage extends Stage {
 
     private void init() {
         setTitle("Authorization");
+        initRegistrationScene();
+        initLoginScene();
+        setScene(registrationScene);
+    }
 
-        HBox top = new HBox();
-        Button registrationButton = new Button("registration");
-        Button loginButton = new Button("login");
-        top.getChildren().addAll(registrationButton, loginButton);
+    private void initLoginScene() {
+        Button switchToRegistrationModeButton = new Button("go to registration page");
+        switchToRegistrationModeButton.setOnAction( (e) -> {
+            this.setScene(registrationScene);
+        });
 
+        Label info = new Label("Please login");
+
+        HBox login = new HBox();
+        login.setAlignment(Pos.CENTER);
+        login.setSpacing(20);
+        Label loginLabel = new Label("User name: ");
+        TextField loginField = new TextField();
+        login.getChildren().addAll(loginLabel, loginField);
+
+        HBox password = new HBox();
+        password.setAlignment(Pos.CENTER);
+        password.setSpacing(20);
+        Label passwordLabel = new Label("Password: ");
+        PasswordField passwordField = new PasswordField();
+        password.getChildren().addAll(passwordLabel, passwordField);
+
+        Button makeRegistrationButton = new Button("make login");
 
         VBox mainPane = new VBox();
-        mainPane.getChildren().add(top);
+        mainPane.setAlignment(Pos.CENTER);
+        mainPane.setSpacing(30);
+        mainPane.getChildren().addAll(switchToRegistrationModeButton, info, login, password, makeRegistrationButton);
 
+        loginScene = new Scene(mainPane, 400, 300);
+    }
+
+    private void initRegistrationScene() {
+        Button switchToLoginModeButton = new Button("go to login page");
+        switchToLoginModeButton.setOnAction( (e) -> {
+            this.setScene(loginScene);
+        });
+        Label info = new Label("Please create you account ");
+
+        HBox login = new HBox();
+        login.setAlignment(Pos.CENTER);
+        login.setSpacing(20);
+        Label loginLabel = new Label("User name: ");
+        TextField loginField = new TextField();
+        login.getChildren().addAll(loginLabel, loginField);
+
+        HBox password = new HBox();
+        password.setAlignment(Pos.CENTER);
+        password.setSpacing(20);
+        Label passwordLabel = new Label("Password: ");
+        PasswordField passwordField = new PasswordField();
+        password.getChildren().addAll(passwordLabel, passwordField);
+
+        Button makeRegistrationButton = new Button("make registration");
+
+        VBox mainPaneRegistrationScene = new VBox();
+        mainPaneRegistrationScene.setAlignment(Pos.CENTER);
+        mainPaneRegistrationScene.setSpacing(30);
+        mainPaneRegistrationScene.getChildren().addAll(switchToLoginModeButton, info, login, password, makeRegistrationButton);
+
+        registrationScene = new Scene(mainPaneRegistrationScene, 400, 300);
     }
 }
